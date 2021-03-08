abb6a72de3671adacfaefbd9e9979f2f156f4487
merytekle <meronteklehaimanot@gmail.com>
2021-03-07 18:45:21 -0800

    MyCommodityApplication.java

---
+++ src/main/java/com/ecxt/myCommodity/MyCommodityApplication.java
@@ -0,0 +1,13 @@
+package com.ecxt.myCommodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommodityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCommodityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecxt/myCommodity/MyCommodityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecxt.myCommodity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCommodityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
